35ef207cf7b7e2bb5422ff430d6374ee9eb4473b
d4niel <olayajuandaniel8@gmail.com>
2022-08-19 09:55:56 -0500

    Migraciones iniciales

---
+++ database/migrations/2022_08_19_144631_create_ordenes_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('ordenes', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('id_user')->constrained('usuarios');
+            $table->timestamp('fecha');
+            $table->float('valor');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('ordenes');
+    }
+};
